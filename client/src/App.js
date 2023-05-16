ede23ef9863ae4550b79480dbce41dad6987fb47
Vimukthi139 <it20152314@my.sliit.lk>
2023-05-16 19:12:49 +0530

    create front-end

---
+++ client/src/App.js
@@ -0,0 +1,124 @@
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import Register from "./Pages/Register/Register";
+import PageNotFound from "./Pages/PageNotFound/PageNotFound";
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import Resource from "./Pages/ResourceCenter/Resource";
+import Sidebar from "./components/SideBar/Sidebar";
+import AddResource from "./Pages/AddResource/AddResource";
+import EditPost from "./Pages/ResourceCenter/editpost";
+import SingleAdminPost from "./Pages/AdminPost/SingleAdminPost";
+import Aresource from "./Pages/ResourceCenter/clientresource";
+import SinglePost from "./Pages/SinglePost/SinglePost";
+import Services from "./Pages/Servicesyug/Servicess"
+import { AuthorizeUser } from "./middleware/auth";
+import { Authorized } from "./middleware/auth";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <AuthorizeUser>
+        <Home />
+      </AuthorizeUser>
+    ),
+  },
+  {
+    path: "/login",
+    element: (
+      <Authorized>
+        <Login />
+      </Authorized>
+    ),
+  },
+  {
+    path: "/register",
+    element: <Register></Register>,
+  },
+  {
+    path: "/dashboard",
+    element: (
+      <AuthorizeUser>
+        <Dashboard />
+      </AuthorizeUser>
+    ),
+  },
+  {
+    path: "/resource",
+    element: (
+      <AuthorizeUser>
+        <Resource />
+      </AuthorizeUser>
+    ),
+  },
+  {
+    path: "/side",
+    element: (
+      <AuthorizeUser>
+        <Sidebar />
+      </AuthorizeUser>
+    ),
+  },
+  {
+    path: "/resource/addresource",
+    element: (
+      <AuthorizeUser>
+        <AddResource />
+      </AuthorizeUser>
+    ),
+  },
+  {
+    path: "/resource/asinglepost/:id",
+    element: (
+      <AuthorizeUser>
+        <SingleAdminPost />
+      </AuthorizeUser>
+    ),
+  },
+    {
+    path: "/resource/edit/:id",
+    element: (
+      <AuthorizeUser>
+        <EditPost />
+      </AuthorizeUser>
+    ),
+  },
+  {
+    path: "/cresource",
+    element: (
+      <AuthorizeUser>
+        <Aresource />
+      </AuthorizeUser>
+    ),
+  },
+  {
+    path: "cresource/singlepost/:id",
+    element: (
+      <AuthorizeUser>
+        <SinglePost />
+      </AuthorizeUser>
+    ),
+  },
+  {
+    path: "/services",
+    element: (
+      <Services/>
+      
+      
+    ),
+  },
+  {
+    path: "*",
+    element: <PageNotFound></PageNotFound>,
+  },
+]);
+
+export default function App() {
+  return (
+    <main>
+      <RouterProvider router={router}></RouterProvider>
+    </main>
+  );
+}
---
+++ client/src/Pages/SinglePost/SinglePost.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from "react";
+import Layout from "../../components/Layout/Layout";
+import { formatISO9075 } from "date-fns";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
+import jwt_decode from "jwt-decode";
+import Nav from "../../components/Nav/Nav";
+import Fotter from "../../components/Fotter/Fotter";
+
+function SinglePost() {
+  const [postInfo, setPostInfo] = useState();
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/resource/postclient/${id}`).then(
+      (response) => {
+        response.json().then((postInfo) => {
+          setPostInfo(postInfo);
+        });
+      }
+    );
+  });
+
+  console.log(postInfo);
+
+  if (!postInfo) return "";
+  return (
+    <>
+    <Nav/>
+      <div className="w-full p-4 ">
+        <article class="py-12 px-4 ">
+          <h1 class="text-4xl text-center mb-4 font-semibold font-heading font-semibold text-blue-900 mt-[80px]  ">
+            {postInfo.title}
+          </h1>
+          <div className="flex flex-row">
+            <div className="flex-1">
+              <img
+                src={"http://localhost:5000/" + postInfo.cover}
+                className=" h-[300px] fixed ml-10 shadow-black rounded-lg md"
+              />
+            </div>
+            <div className="p-1 flex-1">
+              <p class=" mt-3 text-xl text-center">
+                <span>{postInfo.summary}</span>
+              </p>
+              <div
+                class="max-w-3xl mx-auto mt-3"
+                className="content"
+                dangerouslySetInnerHTML={{ __html: postInfo.content }}
+              />
+              <p class=" mt-3">
+                <span>{formatISO9075(new Date(postInfo.createdAt))}</span>
+              </p>
+            </div>
+          </div>
+        </article>
+      </div>
+      <Fotter/>
+    </>
+  );
+}
+
+export default SinglePost;
