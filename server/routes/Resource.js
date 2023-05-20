4aaa7098524e5516693804e05c8f947258fa3d06
IT20659530 <it20659530@my.sliit.lk>
2023-05-20 08:48:44 +0530

    Update Resource.js

--- server/routes/Resource.js
+++ server/routes/Resource.js
@@ -14,6 +14,29 @@ router.get("/post", async (req, res) => {
   res.json(await Resource.find().sort({ createdAt: -1 }).limit(20));
 });
 
+router.post("/post", uploadMiddleware.single("file"), async (req, res) => {
+  const { originalname, path } = req.file;
+  const parts = originalname.split(".");
+  const ext = parts[parts.length - 1];
+  const newPath = path + "." + ext;
+  fs.renameSync(path, newPath);
+
+  const { title, summary, content } = req.body;
+
+  if (!title || !summary || !content) {
+    return res.status(400).json({ message: " fields are empty" });
+  }
+  const postDoc = await Resource.create({
+    title,
+    summary,
+    content,
+    cover: newPath,
+  });
+  res
+    .status(201)
+    .json({ message: "Resource created successfully", data: postDoc });
+});
+
 
 
 module.exports = router;
