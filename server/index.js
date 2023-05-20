4a820b413e09f31e1803d2bbe6e727308d688866
IT20659530 <it20659530@my.sliit.lk>
2023-05-20 09:17:24 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,6 +4,24 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
+app.use("/uploads", express.static(__dirname + "/uploads"));
+app.use(express.json());
+app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("Server Running");
+});
+
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(process.env.MONGOURI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    writeConcern: { w: "majority", j: true, wtimeout: 1000 },
+  })
+  .then(() => console.log("Database Successfully Connected"))
+  .catch((error) => console.log(error));
+
+const port = process.env.PORT || 500;
 
 module.exports = app;
