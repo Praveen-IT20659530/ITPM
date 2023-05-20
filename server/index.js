dfd0de3d40d1c6ea0c0d7bca8be1b6a733a1fdf6
IT20659530 <it20659530@my.sliit.lk>
2023-05-20 09:16:38 +0530

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+
+module.exports = app;
