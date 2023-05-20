fb8078f1875145a9e8dcab095fc495e827457e40
IT20659530 <it20659530@my.sliit.lk>
2023-05-20 07:25:26 +0530

    Update EditServicess.js

--- src/Pages/Servicesyug/EditServicess.js
+++ src/Pages/Servicesyug/EditServicess.js
@@ -18,7 +18,37 @@ function EditServicess() {
     setKwh(quantity * h * p);
   }
 
-  
+  useEffect(() => {
+    fetch(`http://localhost:5000/audit/audit/${id}`).then((response) => {
+      response.json().then((audit) => {
+        setAppliance(audit.appliance);
+        setQuantity(audit.quantity);
+        setH(audit.h);
+        setP(audit.p);
+        setKwh(audit.kwh);
+      });
+    });
+  }, []);
+
+  async function update(ev) {
+    ev.preventDefault();
+    const res = await fetch("http://localhost:5000/audit/audit/update", {
+      method: "PUT",
+      body: JSON.stringify({ appliance, quantity, h, p, kwh, id }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (res.status === 400) {
+      toast.error("fields are empty");
+    } else if (res.status === 201) {
+      toast.success("Update Successfully");
+      setTimeout(() => {
+        navigate("/serviceslist");
+      }, 1000);
+    } else {
+      toast.error("Server error");
+    }
+  }
             Update
           </button>
         </div>
