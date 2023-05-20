a17637c6c065f8b0d59a09ccee8accca93466cd0
shashinidilshika <it20641856@my.sliit.lk>
2023-05-20 12:41:17 +0530

    updated companies.js file
    backend

--- backend/routes/companies.js
+++ backend/routes/companies.js
@@ -2,10 +2,8 @@ const router = require("express").Router();
 const Company = require("../models/company");
 let company = require("../models/company");
 
+
 //add data
-
-http://localhost:8070/company/add
-
 router.route("/add").post((req,res)=>{
 
     const Name = req.body.Name;
@@ -30,47 +28,25 @@ router.route("/add").post((req,res)=>{
 
 })
 
-//Retrive all
 
-http://localhost:8070/company
-
-router.get("/all",(req,res)=>{
-    company.find().exec((err,companies)=>{
-        if(err){
-            return res.status(400).json({
-                error:err
-            });
-        }
-
-        return res.status(200).json({
-            success:true,
-            existingCompanies:companies
-        });
-    });
-});
-
-
-//Retrive by ID 
+//Get a company by using ID
 router.route("/get/:id").get(async(req,res)=>{
-    const com = await company.findById(req.params.id);
+  const dr = await Company.findById(req.params.id);
 
-  if (com) {
-    res.json(com);
-  } else {
-    res.status(404).json({ message: "Company not found" });
-  }
+if (dr) {
+  res.json(dr);
+} else {
+  res.status(404).json({ message: "Company not found" });
+}
 })
 
-   module.exports = router;
+
 
 //Update 
 
-http://localhost:8070/company/update/5isadfasddafs
-
-
 router.route("/update/:id").patch(async(req,res)=>{
 
-    const com = await company.findById(req.params.id);
+    const com = await Company.findById(req.params.id);
 
   if (com) {
     com.Name = req.body.Name || com.Name;
@@ -92,18 +68,13 @@ router.route("/update/:id").patch(async(req,res)=>{
     
     throw new Error("Can't Update Company Details");
   }
-
-
 })
 
 //Delete
-
-http://localhost:8070/company/delete/
-
 router.route("/delete/:id").delete(async(req, res) => {
-    let userId = req.params.id;
+    let companyID = req.params.id;
 
-    await company.findByIdAndDelete(userId).then(()=>{
+    await Company.findByIdAndDelete(companyID).then(()=>{
       res.status(200).send({status: "company deleted"});
     }).catch((err)=>{
       console.log(err.message);
@@ -111,16 +82,33 @@ router.route("/delete/:id").delete(async(req, res) => {
     })
 })
 
-router.route("/get/:id").get(async(req,res) => {
-    let userId = req.params.id;
+router.get('/all',(_req,res)=>{
 
-    const user = await company.findById(userId).then((company)=>{
-        res.status(200).send({status: "User fetched", company})
-    }).catch((err)=>{
-        console.log(err.message);
-        res.status(500).send({status: "Error with get user", error:err.message});
-    })
+  Company.find().exec((err,companies)=>{
+      if(err){
+    return res.status(400).json({
+     error:err
 
-})
+
+
+    });
+
+
+   }
+
+    return res.status(200).json({
+
+        success:true,
+        existingCusPacks:companies
+
+
+    });
+
+
+  });
+
+
+
+});
 
 module.exports = router;
