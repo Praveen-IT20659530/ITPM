429e35ee9b57978c1676a9fa3881e596056cf805
Randi Jayakody <it20643768@my.sliit.lk>
2023-05-22 18:45:02 +0530

    Update electricity.js

--- BACKEND/models/electricity.js
+++ BACKEND/models/electricity.js
@@ -1,39 +1,46 @@
 const mongoose = require('mongoose');
-require('mongoose-double')(mongoose);
+//const router = require('../routes/packages');
 
-const Schema = mongoose.Schema;
-const SchemaTypes = mongoose.Schema.Types;
 
-const electricitySchema = new Schema({
-  pannelSize: {
-    type: SchemaTypes.Double,
-    required: true
-  },
-  company: {
-    type: String,
-    required: true
-  },
-  intensity: {
-    type: Number,
-    required: true
-  },
-  monthlyBill: {
-    type: SchemaTypes.Double,
-    required: true
-  },
-  units: {
-    type: Number,
-    required: true
-  },
-  irradiance: {
-    type: SchemaTypes.Double,
-    default: 5.0
-  },
-  conversionEfficiency: {
-    type: SchemaTypes.Double,
-    default: 0.8
-  }
-});
 
-const electricity = mongoose.model('Electricity', electricitySchema);
-module.exports = electricity;
+//Create Database Scheams
+const ElectricitySchema = new mongoose.Schema({
+    pannelSize: {
+      type: Number,
+      required: true
+    },
+    company: {
+      type: String,
+      required: true
+    },
+    intensity: {
+      type: Number,
+      required: true
+    },
+    monthlyBill: {
+      type: Number,
+      required: true
+    },
+    units: {
+      type: Number,
+      required: true
+    },
+    irradiance: {
+      type: Number,
+      default: 0.05
+    },
+    conversionEfficiency: {
+      type: Number,
+      default: 0.08
+    },
+    monthlyGeneratedElectricity: {
+        type: Number
+        
+      },
+      
+      
+  });
+//Create a module for connect with scheama
+const Electricity = mongoose.model("Electricity",ElectricitySchema);
+
+module.exports=Electricity;
