ac9e5b2824d86a6b06c0be349cb79ef21b84ee99
Randi Jayakody <it20643768@my.sliit.lk>
2023-05-22 18:53:58 +0530

    Update EditElectricity.js

--- frontend/src/components/EditElectricity.js
+++ frontend/src/components/EditElectricity.js
@@ -1,13 +1,15 @@
 import React,{useEffect, useState} from "react"
 import axios from "axios";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import electricity from "../../../BACKEND/models/electricity";
+import "./../style2.css";
+
+
 
 
 
 export default function EditElectricity(){
 
-    const [pannelSize,setpannelSize] = useState("");
+  const [pannelSize,setpannelSize] = useState("");
     const [company,setcompany] = useState("");
     const [intensity,setintensity] = useState("");
     const [monthlyBill,setmonthlyBill] = useState("");
@@ -15,53 +17,57 @@ export default function EditElectricity(){
     const [irradiance,setirradiance] = useState("");
     const [conversionEfficiency,setconversionEfficiency] = useState("");
 
-  useEffect(() => {
-      getElectricities();
-    }, []);
-  
-    //let navigate= useNavigate();
-  
-    function getElectricities() {
-      let hotel = true;
-  
-      fetch(`http://localhost:8060/electricity/get/${id}`)
-        .then((res) => res.json())
-  
-        .then((result) => {
-          if (electricity) {
-            setpannelSize(result.pannelSize);
-            setcompany(result.company);
-            setintensity(result.intensity);
-            setmonthlyBill(result.monthlyBill);
-            setunits(result.setunits);
-           
-           
-          }
-          console.log(result);
-        });
-  
-      return () => (electricity = false);
-    }
+  const { id } = useParams();
 
-  
-  function EditElectricity(e){
+  useEffect(() => {
+    getElectricities();
+  }, []);
+
+
+  function getElectricities() {
+    let electricity = true;
+
+// retriew data for id
+
+    fetch(`http://localhost:8070/electricity/get/${id}`)
+      .then((res) => res.json())
+
+      .then((result) => {
+        if (electricity) {
+          setpannelSize(result.pannelSize);
+          setcompany(result.company);
+          setintensity(result.intensity);
+          setmonthlyBill(result.monthlyBill);
+          setunits(result.setunits);
+         
+        }
+        console.log(result);
+      });
+
+    return () => (electricity = false);
+  }
+
+   // update data 
+  function updateData(e){
 
       e.preventDefault();
 
       //alert("Insert");
 
       const updateElectricity ={
-    pannelSize,
+        pannelSize,
     company,
     intensity,
     monthlyBill,
     units,
     irradiance,
-    conversionEfficiency
+    conversionEfficiency,
+     
+    
       }
 
       axios
-    .patch(`http://localhost:8060/electricity/update/${id}`, updateElectricity)
+    .patch(`http://localhost:8070/electricity/update/${id}`, updateElectricity)
     .then((_res) => {
       alert("Data Updated Successfully!");
      // navigate("/front");
@@ -72,86 +78,82 @@ export default function EditElectricity(){
       alert("Database Error");
     });
   }
-<div className="container" class="back-img">
-     <div class ="card-adding">
-    <form onSubmit={sendData}>
-      <div class="text-dark">
-         <center><h1><strong><u>SAVING POWER AND MONEY</u></strong></h1></center><br></br>
-      </div>
-    <div className="form-group">
-      <label for="pannelSize"><strong>Pannel Size </strong></label>
-      <input type="text" class="form-control" id="pannelSize"
-      onChange={(e)=> {
 
-        setpannelSize(e.target.value);
+return(
 
-       } }/>
-  
-    </div><br></br>
-    
-    <div className="form-group">
-  <label htmlFor="company"><strong>Company</strong></label>
-  <select className="form-control" id="company" onChange={(e) => setcompany(e.target.value)}>
-    <option value="" disabled defaultValue>Select Company</option>
+  <div className="container" class="back-img">
+  <div class ="card-company">
+
+<div class="text-dark">
+   <center><h1><strong><u> SAVING POWER AND MONEY </u></strong></h1></center><br></br>
+</div>
+
+<form className =  "company" >
+<div className="form-group">
+<label for="pannelSize"><strong>Pannel Size</strong></label>
+<input type="text" class="form-control" id="pannelSize" required 
+ value={pannelSize}
+onChange={(e)=>{setpannelSize(e.target.value);}}
+/>
+</div>
+<br></br>
+
+<div className="form-group">
+  <label htmlFor="company">
+    <strong>Company</strong>
+  </label>
+  <select
+    className="form-control"
+    id="company"
+    required
+    value={company}
+    onChange={(e) => setcompany(e.target.value)}
+  >
+    <option value="" disabled defaultValue>
+      Select a Company
+    </option>
     <option value="Company A">Company A</option>
     <option value="Company B">Company B</option>
     <option value="Company C">Company C</option>
   </select>
 </div>
+<br></br>
 
-    <div className="form-group">
-      <label for="intensity"><strong>Availability Of Intensity</strong></label>
-      <input type="text" class="form-control" id="intensity" 
-      onChange={(e)=> {
+<div className="form-group">
+<label for="intensity"><strong>Availability Of Intensity</strong></label>
+<input type="int" class="form-control" id="intensity" required 
+ value={intensity}
+onChange={(e)=> {setintensity(e.target.value);} }
+/>
+</div>
+<br></br>
 
-        setintensity(e.target.value);
-
-       } }/>
-      
-  
-    </div><br></br>
-
-    <div className="form-group">
-      <label for="monthlyBill"><strong>Last Month Electricity Bill</strong></label>
-      <input type="text" class="form-control" id="monthlyBill" 
-      onChange={(e)=> {
-
-        setmonthlyBill(e.target.value);
-
-       } }/>
-
-    
-    </div><br></br>
-   
-    <div className="form-group">
+<div className="form-group">
+<label for="monthlyBill"><strong>Last Month Electricity Bill</strong></label>
+<input type="text" class="form-control" id="Email" required  value={monthlyBill}
+onChange={(e)=> {setmonthlyBill(e.target.value);} }
+/>
+</div>
+<br></br>
+<div className="form-group">
       <label for=" units
 "><strong>Units</strong></label>
-      <input type="text" class="form-control" id="units" 
-      onChange={(e)=> {
+      <input type="int" class="form-control" id="units" required  value={units}
+onChange={(e)=> {setunits(e.target.value);} }
+ />
+ </div><br></br>
 
-        setmonthlyBill(e.target.value);
+<div class="d-grid gap-2 col-6 mx-auto">
+<button type="button" class="btn btn-warning" onClick={updateData}><strong> Update Details </strong></button>
+<div>
+<a href="/all" class="btn btn-warning" style={{width:"300px"}}><strong>View Details</strong></a>
+</div>
+</div>
 
-       } }/>
+</form>
+</div>
+</div>
 
-    
-    </div><br></br>
+)
 
-  
-    
-    <div class="d-grid gap-2 col-6 mx-auto">
-    <button type="submit" className="btn btn-primary"><strong>SUBMIT</strong></button>
-
-    <div>
-    <a href="/home" class="btn btn-primary" style={{width:"300px"}}><strong>CALCULATE</strong></a>
-            </div>
-    </div>
-    
-   </form>
-   </div>
-   </div>
-
- 
- 
-
-} 
-
+}
