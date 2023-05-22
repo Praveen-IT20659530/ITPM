0f57a91fc9df0a034e4c20946e276b6bebd922c0
Randi Jayakody <it20643768@my.sliit.lk>
2023-05-22 18:47:39 +0530

    Update server.js

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,27 +1,35 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const app = express();
+const express=require("express");
+const mongoose=require("mongoose");
+const bodyParser=require("body-parser");
+const cors=require("cors");
+const app=express();
 require("dotenv").config();
 
-const PORT = process.env.PORT || 8060;
+
+const PORT = process.env.PORT || 8070;
+
 app.use(cors());
 app.use(bodyParser.json());
 
-const URL = process.env.MONGODB_URL;
+
+const URL=process.env.MONGODB_URL;
+
 mongoose.connect(URL, {
-    //useCreateIndex: true,
-    useNewUrlParser: true,
-    //useUnifiedTopologyL: true,
-    //useFindAndModify: false
-});
+
+    //useCreateIndex : true,
+    useNewUrlParser : true,
+    //useUnifiedTopologyL:true,
+    //useFindAndModify : false
+
+}); 
+
 
 const connection = mongoose.connection;
-connection.once("open", () =>{
-    console.log("Mongodb Connection Success!");
-})
+connection.once('open',() => {
+ console.log("Mongodb connection success!");
+
+}) 
+
 
 const electricityRouter = require ("./routes/electricities.js");
 app.use("/electricity",electricityRouter);
